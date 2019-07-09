7fa77d98bb1ea6611c94c89245827fd2e45a789e
fenshitianyue <abzanda@163.com>
2019-07-09 22:25:32 +0800

    update code

---
+++ src/main.cc
@@ -0,0 +1,20 @@
+﻿#include "base.h"
+
+int main()
+{
+	char ch = '\0';
+
+	do
+	{
+		InitVariables();
+		GetInput(); // get input information
+		CalcInfo();
+		CreateCache();
+		FileTest();
+		PrintOutput(); // output the result
+		std::cout << "Continue/Exit(C/E)" << std::endl;
+		std::cin >> ch;
+	} while ((ch != 'e') && (ch != 'E'));
+
+	return 0;
+}
