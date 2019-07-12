03e53c65e959c4d31904a8a2980f875a85d51e2c
fenshitianyue <abzanda@163.com>
2019-07-12 16:38:37 +0800

    delete some comment

--- src/FileTest.cc
+++ src/FileTest.cc
@@ -1,10 +1,9 @@
 #include "base.h"
 
 void FileTest(void){
-  char filepath[100] = "./test_file/gcc.trace";
-  bool flag = 0;
+  char filepath[100] = "./test_file/test_1.trace";
+  bool flag;
   std::ifstream in_file;
-  //std::ofstream out_put;
   char address[13];
   std::cout << std::endl;
   std::cout << "Please input the path and name that you want to test!" << std::endl;
@@ -18,7 +17,6 @@ void FileTest(void){
     std::cin >> filepath;
   } 
   in_file.open(filepath, std::ios::in);
-  //out_put.open("test.log", std::ios::out);
   while(in_file.fail()){
     std::cout << "Open error! Please check the path and name, and input again!" << std::endl; 
     std::cout << ">"; 
@@ -28,11 +26,9 @@ void FileTest(void){
 
   while(!in_file.eof()){
     in_file.getline(address, 13);
-    //out_put << address << std::endl;
     GetHitNum(address);
   }
   in_file.close();
-  //out_put.close();
   GetHitRate();
 }
 
